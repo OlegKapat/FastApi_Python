76a4d9cb16a8f66355b8821df64ba461d0a534db
OlehKapat <bombastik@ukr.net>
2026-03-23 16:25:56 +0200

    add settings for database

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,15 +1,31 @@
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 
+
 class CoreSettings(BaseSettings):
     APP_NAME: str = "FastAPI Application"
-    DEBUG: bool = False
+    DEBUG: bool = True
 
-class Settings(CoreSettings):
+
+class PostgresSettings(BaseSettings):
+    PGHOST: str
+    PGDATABASE: str
+    PGUSER: str
+    PGPASSWORD: str
+    PORT: int = 5432
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.PGUSER}:{self.PGPASSWORD}@{self.PGHOST}:{self.PORT}/{self.PGDATABASE}"
+
+
+class Settings(CoreSettings, PostgresSettings):
     pass
+
 
 @lru_cache(maxsize=None)
 def get_settings() -> Settings:
     return Settings()
+
 
 settings = get_settings()
