608cd8557d4854146859955abc606e36ae88bd0f
OlehKapat <bombastik@ukr.net>
2026-03-24 18:14:08 +0200

    add schemas and tests

--- backend/app/apps/info/router.py
+++ backend/app/apps/info/router.py
@@ -1,17 +1,18 @@
 import socket
 from fastapi import APIRouter
 from settings import settings
+from .schemas import BaseBackendInfoSchema,DatabaseInfoSchema
 
 info_router = APIRouter()
 
 
 
 @info_router.get("/backend")
-async def get_backend():
+async def get_backend()->BaseBackendInfoSchema:
     """Get data from backend"""
-    return {"Return from backend": socket.gethostname()}
+    return BaseBackendInfoSchema(backend=socket.gethostname())
 
 @info_router.get("/database")
-async def get_database():
+async def get_database()->DatabaseInfoSchema:
     """Get data from database"""
-    return {"Return from database_url": settings.DATABASE_ASYNC_URL}
+    return DatabaseInfoSchema(database_url=settings.DATABASE_ASYNC_URL)
