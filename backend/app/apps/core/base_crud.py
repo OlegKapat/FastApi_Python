ec3a6d202d5e5abe9e29fbbf83995c2566ffb712
OlehKapat <bombastik@ukr.net>
2026-03-27 16:54:00 +0200

    chack user email

--- backend/app/apps/core/base_crud.py
+++ backend/app/apps/core/base_crud.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
-
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from apps.core.base_models import BaseModel
-from typing import Optional
+from typing import Optional, Any
 from fastapi import HTTPException, status
+from sqlalchemy.orm import InstrumentedAttribute
+from sqlalchemy import and_, asc, delete, desc, exists, func, or_, select, update
 
 
 class BaseCrudManagerl(ABC):
@@ -14,7 +14,7 @@ class BaseCrudManagerl(ABC):
     def __init__(self):
         pass
 
-    async def create_instance(self, *, session: AsyncSession, **kwargs) -> Optional[BaseModel]:
+    async def create(self, *, session: AsyncSession, **kwargs) -> Optional[BaseModel]:
         instance = self.model(**kwargs)
         session.add(instance)
         try:
@@ -25,3 +25,8 @@ class BaseCrudManagerl(ABC):
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
         finally:
             await session.close()
+
+    async def get(self, *, session: AsyncSession, field_value: Any,field:InstrumentedAttribute) -> Optional[BaseModel]:
+       query = select(self.model).filter(field == field_value)
+       result = await session.execute(query)
+       return result.scalar_one_or_none()
--- backend/app/apps/users/crud.py
+++ backend/app/apps/users/crud.py
@@ -1,23 +1,28 @@
-
-
 from apps.core.base_crud import BaseCrudManagerl
 from .models import User
 from .schemas import RegisteredUserSchema, UserRegistrationSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 from apps.auth.password_handler import PasswordEncrypt
+from fastapi import HTTPException, status
 
 
 class UserCrudManager(BaseCrudManagerl):
     def __init__(self):
         self.model = User
 
-    async def create_user(self, new_user: RegisteredUserSchema, session: AsyncSession)->User:
+    async def create_user(self, new_user: RegisteredUserSchema, session: AsyncSession) -> User:
+        maybe_user = await self.get(session=session, field_value=new_user.email, field=self.model.email)
+        if maybe_user:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"User with email {new_user.email} already exists", )
+
         hashed_password = await PasswordEncrypt.get_password_hash(new_user.password)
-        return await self.create_instance(
+        return await self.create(
             session=session,
             email=new_user.email,
             hashed_password=hashed_password,
             name=new_user.name,
         )
+
 
 user_manager = UserCrudManager()
