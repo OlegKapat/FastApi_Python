6760ff9a54606de4040ec2fad1a64eef06b3e04f
OlehKapat <bombastik@ukr.net>
2026-03-30 19:12:29 +0300

    sentry loggining

--- backend/app/apps/info/router.py
+++ backend/app/apps/info/router.py
@@ -1,16 +1,26 @@
 import socket
 from fastapi import APIRouter
 from settings import settings
-from .schemas import BaseBackendInfoSchema,DatabaseInfoSchema
+from .schemas import BaseBackendInfoSchema, DatabaseInfoSchema
+import logging
 
 info_router = APIRouter()
 
+
 @info_router.get("/backend")
-async def get_backend()->BaseBackendInfoSchema:
+async def get_backend() -> BaseBackendInfoSchema:
     """Get data from backend"""
+    logging.error(
+        "some info",
+        extra={
+            "user_id": 123,
+            "debug_info": {"function": "get_backend_info", "status": "OK"},
+        },
+    )
     return BaseBackendInfoSchema(backend=socket.gethostname())
 
+
 @info_router.get("/database")
-async def get_database()->DatabaseInfoSchema:
+async def get_database() -> DatabaseInfoSchema:
     """Get data from database"""
     return DatabaseInfoSchema(database_url=settings.DATABASE_ASYNC_URL)
--- backend/app/apps/services/sentry_service.py
+++ backend/app/apps/services/sentry_service.py
@@ -1,8 +1,21 @@
 from fastapi import HTTPException, status
 from sentry_sdk import capture_message
 from typing import NoReturn
+from settings import settings
+from sentry_sdk.integrations.logging import LoggingIntegration
+import sentry_sdk
+import logging
 
 
 def capture_error(message: str, user_massage: str = "General error. Call support") -> NoReturn:
     capture_message(message, level="error")
-    raise HTTPException(detail=user_massage,status_code=status.HTTP_400_BAD_REQUEST)
+    raise HTTPException(detail=user_massage, status_code=status.HTTP_400_BAD_REQUEST)
+
+
+def init_sentry():
+    sentry_logging = LoggingIntegration(event_level=logging.ERROR)
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DSN,
+        send_default_pii=True,
+        integrations=[sentry_logging],
+    )
