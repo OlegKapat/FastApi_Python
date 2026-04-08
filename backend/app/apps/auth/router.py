6aab46d6fecf0d3579d3725e7ff662965a5cfca8
OlehKapat <bombastik@ukr.net>
2026-04-08 14:00:48 +0300

    force logout

--- backend/app/apps/auth/router.py
+++ backend/app/apps/auth/router.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Header, Depends
+from fastapi import APIRouter, Header, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from apps.core.dependencies import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from .schemas import LoginResposeSchema
+from .schemas import LoginResposeSchema, ForceLogoutSchema
 from apps.auth.auth_handler import auth_handler
+from .dependencies import get_current_user
+from apps.users.models import User
+from apps.users.crud import user_manager
 
 router_auth = APIRouter()
 
@@ -23,3 +26,12 @@ async def refresh_user_token(refresh_token: str = Header(alias='X-Refresh-Token'
                              session: AsyncSession = Depends(get_async_session)) -> LoginResposeSchema:
     token_pair = await auth_handler.get_refresh_token_pair(refresh_token=refresh_token, session=session)
     return token_pair
+
+
+@router_auth.post("/force-logout", status_code=status.HTTP_204_NO_CONTENT)
+async def force_logout(
+        user: User = Depends(get_current_user),
+        session: AsyncSession = Depends(get_async_session)
+):
+    await user_manager.patch(user.id, data_to_patch = ForceLogoutSchema(),
+    session = session,exclude_unset = False)
--- backend/app/apps/auth/schemas.py
+++ backend/app/apps/auth/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+import datetime as dt
 
 class LoginResposeSchema(BaseModel):
     access_token: str
@@ -6,3 +7,5 @@ class LoginResposeSchema(BaseModel):
     expires_in: int
     token_type: str = 'Bearer'
 
+class ForceLogoutSchema(BaseModel):
+    use_token_since: dt.datetime = Field(default_factory=dt.datetime.now)
