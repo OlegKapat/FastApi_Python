e68440220be54bb136db8b227fa655d88b21cc0e
OlehKapat <bombastik@ukr.net>
2026-05-01 19:49:27 +0300

    14-3 get categories

--- backend/app/apps/products/router.py
+++ backend/app/apps/products/router.py
@@ -1,11 +1,18 @@
+from typing import Annotated
+
 from apps.auth.dependencies import require_permision
 from apps.core.dependencies import get_async_session
+from apps.core.schemas import SearchParamSchema
 from apps.users.constants import UserPermisionEnum
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .crud import Category, category_manager
-from .schemas import NewCategory, SavedCategorySchema
+from .schemas import (
+    NewCategory,
+    PaginatorSavedCategoryResponseSchema,
+    SavedCategorySchema,
+)
 
 router_categories = APIRouter()
 
@@ -30,3 +37,33 @@ async def create_category(
         **new_category.model_dump(), session=session
     )
     return saved_category
+
+
+@router_categories.get("/{id}", response_model=SavedCategorySchema)
+async def get_category_by_id(
+    category_id: int = Path(..., description="The id of the item", ge=1, alias="id"),
+    session: AsyncSession = Depends(get_async_session),
+) -> SavedCategorySchema:
+    saved_category = await category_manager.get(
+        field_value=category_id, field=Category.id, session=session
+    )
+    if not saved_category:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Category with id {category_id} not found",
+        )
+    return saved_category
+
+
+@router_categories.get("/")
+async def get_categories(
+    params: Annotated[SearchParamSchema, Depends()],
+    session: AsyncSession = Depends(get_async_session),
+) -> PaginatorSavedCategoryResponseSchema:
+    result = await category_manager.get_items_pagineted(
+        session=session,
+        search_fields=[Category.name],
+        targeted_shema=SavedCategorySchema,
+        params=params,
+    )
+    return result
