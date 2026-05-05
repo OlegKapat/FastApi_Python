f006e67cc6ab57c84f2b8b1a33176d76bfc7e492
OlehKapat <bombastik@ukr.net>
2026-05-05 18:58:23 +0300

    14-5 create products model

--- backend/app/apps/core/base_models.py
+++ backend/app/apps/core/base_models.py
@@ -1,24 +1,51 @@
+import uuid
 from datetime import datetime
 
-from sqlalchemy.orm import DeclarativeBase, Mapper, mapped_column, declared_attr, Mapped
-from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker,AsyncSession
-from sqlalchemy.sql import func
 from settings import settings
+from sqlalchemy.ext.asyncio import (
+    AsyncAttrs,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+from sqlalchemy.sql import func
 
-engine = create_async_engine(settings.DATABASE_ASYNC_URL, future=True, echo=settings.DEBUG,
-                             pool_size=settings.DATABASE_POOL_SIZE, max_overflow=settings.DATABASE_MAX_OVERFLOW,
-                             pool_pre_ping=True,pool_recycle=settings.DATABASE_POOL_RECYCLE)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False,class_=AsyncSession)
+engine = create_async_engine(
+    settings.DATABASE_ASYNC_URL,
+    future=True,
+    echo=settings.DEBUG,
+    pool_size=settings.DATABASE_POOL_SIZE,
+    max_overflow=settings.DATABASE_MAX_OVERFLOW,
+    pool_pre_ping=True,
+    pool_recycle=settings.DATABASE_POOL_RECYCLE,
+)
+async_session_maker = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
 
 
 class BaseModel(AsyncAttrs, DeclarativeBase):
     """в базі не створювалась таблиця base"""
+
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     created_at: Mapped[datetime] = mapped_column(default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), onupdate=func.now()
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return cls.__name__.lower() + 's'
+        return cls.__name__.lower() + "s"
+
+
+class UpdetetAtMixin:
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), onupdate=func.now()
+    )
+
+
+class UUIDMixin:
+    uuid_data: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4)
--- backend/app/apps/products/models.py
+++ backend/app/apps/products/models.py
@@ -1,14 +1,30 @@
-from sqlalchemy.orm import Mapped,mapped_column
-from sqlalchemy import String,Integer
-from apps.core.base_models import BaseModel
-import datetime as dt
+from apps.core.base_models import BaseModel, UpdetetAtMixin, UUIDMixin
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class Category(BaseModel):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
 
-    name : Mapped[str]= mapped_column(String,unique=True,nullable=False)
-    version: Mapped[int] = mapped_column(Integer,nullable=False,default=1)
+    name: Mapped[str] = mapped_column(String(70), unique=True, nullable=False)
+    version: Mapped[int] = mapped_column(Integer, nullable=False, default=1)
+    products = relationship("Product", back_populates="category")
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return f"<Category {self.name} - #{self.id}>"
+
+
+class Product(UpdetetAtMixin, UUIDMixin, BaseModel):
+    title: Mapped[str] = mapped_column(String(200), unique=False)
+    description: Mapped[str] = mapped_column(String(2048), default="")
+    price: Mapped[float] = mapped_column(nullable=False)
+    main_image: Mapped[str] = mapped_column(unique=False)
+    images: Mapped[list[str]] = mapped_column(ARRAY(String), default=list)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id", ondelete="RESTRICT"), nullable=False
+    )
+    category = relationship("Category", back_populates="products")
+
+    def __str__(self) -> str:
+        return f"<Product {self.title} - #{self.id}, current price {self.price}>"
